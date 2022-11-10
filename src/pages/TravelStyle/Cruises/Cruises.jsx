729cc50b1e5779ecfae65bcb38d22dad782a0196
Phu121091 <nguyentrongphuu@gmail.com>
2022-11-10 14:32:41 +0700

    2:30 11/10 Phu

--- src/pages/TravelStyle/Cruises/Cruises.jsx
+++ src/pages/TravelStyle/Cruises/Cruises.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import './Cruises.css'
+import './Cruises.css';
+import TourItemlist from "../../../components/TourItemlist/TourItemlist";
 
 const Cruises = () => {
   return (
@@ -9,7 +10,25 @@ const Cruises = () => {
       {" "}
       A house is built by hands, but a home is built by heart.{" "}
     </div>
-    <div className="cruises-container">a</div>
+    <div className="cruises-container">
+    <div className="page-up">
+          <div className="title-top"> See The World Together</div>
+          <div className="middle-page"> 
+          We don’t believe “let’s have kids” should mean you have to stop exploring the world.
+           We also don’t believe you should get home from a holiday just as tired as when you left.
+            So we have real parents at B&R design and test-drive everything.
+             Our resulting journeys get rave reviews from young and young-at-heart people alike.
+              Nothing’s dumbed-down, everything’s 100% stress-free, 
+              and there’s always as much action and excitement as the kids can handle.
+          </div>
+          <div className="end-page"> Look By Eyes- Feel By Heart </div>
+        </div>
+        
+        <TourItemlist status='rev'/>
+        <TourItemlist status='norev'/>
+        <TourItemlist status='rev'/>
+        <TourItemlist status='norev'/>
+    </div>
   </div>
   )
 }
--- src/pages/TravelStyle/Family/Family.jsx
+++ src/pages/TravelStyle/Family/Family.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Family.css";
 import { RiMoneyDollarCircleLine, Ri24HoursFill } from "react-icons/ri";
+import TourItemlist from "../../../components/TourItemlist/TourItemlist";
 
 const Family = () => {
   return (
@@ -11,7 +12,7 @@ const Family = () => {
         A house is built by hands, but a home is built by heart.{" "}
       </div>
       <div className="family-container">
-        <div className="page-up">
+      <div className="page-up">
           <div className="title-top"> See The World Together</div>
           <div className="middle-page"> 
           We don’t believe “let’s have kids” should mean you have to stop exploring the world.
@@ -23,215 +24,12 @@ const Family = () => {
           </div>
           <div className="end-page"> Look By Eyes- Feel By Heart </div>
         </div>
-        <div className="simpletour-1">
-          <div className="top-container">
-            <div className="box-1"></div>
-            <div className="boxsub-1">
-              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
-              <div className="title-highlight">
-                <ul className="highlight">
-                  <li>
-                    {" "}
-                    Taste Hanoi beer and experience the Vietnamese beer drinking
-                    cultur{" "}
-                  </li>
-                  <li>
-                    Take in a cooking demonstration with the chef to learn about
-                    Vietnamese food, while floating through Halong Bay{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Explore the Imperial Citadel, a UNESCO-designated World
-                    Heritage Site, and sample Hue cuisine for its authentic and
-                    recognizable flavor{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Try the food at the busiest street booths and secret
-                    passageways in Hoi An Town in true native fashion while
-                    riding a motorcycle.{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Experience the vast Mekong Delta's rural villages, farming
-                    practices, and fruit orchards by boat through its winding
-                    canals while sampling local delicacies.
-                  </li>
-                </ul>
-              </div>
-              <div className="read-more">
-                <div className="icon-show">
-                  <div className="money">
-                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
-                  </div>
-                  <div className="day-lenght">
-                    <Ri24HoursFill /> 12 Day
-                  </div>
-                </div>
-                <div className="button-show">
-                  <a href="/"> Show more..</a>
-                </div>
-              </div>
-            </div>
-          </div>
+        
+        <TourItemlist status='rev'/>
+        <TourItemlist status='norev'/>
+        <TourItemlist status='rev'/>
+        <TourItemlist status='norev'/>
         </div>
-        <div className="simpletour-1">
-          <div className="top-container">
-            <div className="boxsub-1">
-              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
-              <div className="title-highlight">
-                <ul className="highlight">
-                  <li>
-                    {" "}
-                    Taste Hanoi beer and experience the Vietnamese beer drinking
-                    cultur{" "}
-                  </li>
-                  <li>
-                    Take in a cooking demonstration with the chef to learn about
-                    Vietnamese food, while floating through Halong Bay{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Explore the Imperial Citadel, a UNESCO-designated World
-                    Heritage Site, and sample Hue cuisine for its authentic and
-                    recognizable flavor{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Try the food at the busiest street booths and secret
-                    passageways in Hoi An Town in true native fashion while
-                    riding a motorcycle.{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Experience the vast Mekong Delta's rural villages, farming
-                    practices, and fruit orchards by boat through its winding
-                    canals while sampling local delicacies.
-                  </li>
-                </ul>
-              </div>
-              <div className="read-more">
-                <div className="icon-show">
-                  <div className="money">
-                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
-                  </div>
-                  <div className="day-lenght">
-                    <Ri24HoursFill /> 12 Day
-                  </div>
-                </div>
-                <div className="button-show">
-                  <a href="/"> Show more..</a>
-                </div>
-              </div>
-            </div>
-            <div className="box-2"></div>
-          </div>
-        </div>
-        <div className="simpletour-1">
-          <div className="top-container">
-            <div className="box-3"></div>
-            <div className="boxsub-1">
-              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
-              <div className="title-highlight">
-                <ul className="highlight">
-                  <li>
-                    {" "}
-                    Taste Hanoi beer and experience the Vietnamese beer drinking
-                    cultur{" "}
-                  </li>
-                  <li>
-                    Take in a cooking demonstration with the chef to learn about
-                    Vietnamese food, while floating through Halong Bay{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Explore the Imperial Citadel, a UNESCO-designated World
-                    Heritage Site, and sample Hue cuisine for its authentic and
-                    recognizable flavor{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Try the food at the busiest street booths and secret
-                    passageways in Hoi An Town in true native fashion while
-                    riding a motorcycle.{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Experience the vast Mekong Delta's rural villages, farming
-                    practices, and fruit orchards by boat through its winding
-                    canals while sampling local delicacies.
-                  </li>
-                </ul>
-              </div>
-              <div className="read-more">
-                <div className="icon-show">
-                  <div className="money">
-                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
-                  </div>
-                  <div className="day-lenght">
-                    <Ri24HoursFill /> 12 Day
-                  </div>
-                </div>
-                <div className="button-show">
-                  <a href="/"> Show more..</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="simpletour-1">
-          <div className="top-container">
-            <div className="boxsub-1">
-              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
-              <div className="title-highlight">
-                <ul className="highlight">
-                  <li>
-                    {" "}
-                    Taste Hanoi beer and experience the Vietnamese beer drinking
-                    cultur{" "}
-                  </li>
-                  <li>
-                    Take in a cooking demonstration with the chef to learn about
-                    Vietnamese food, while floating through Halong Bay{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Explore the Imperial Citadel, a UNESCO-designated World
-                    Heritage Site, and sample Hue cuisine for its authentic and
-                    recognizable flavor{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Try the food at the busiest street booths and secret
-                    passageways in Hoi An Town in true native fashion while
-                    riding a motorcycle.{" "}
-                  </li>
-                  <li>
-                    {" "}
-                    Experience the vast Mekong Delta's rural villages, farming
-                    practices, and fruit orchards by boat through its winding
-                    canals while sampling local delicacies.
-                  </li>
-                </ul>
-              </div>
-              <div className="read-more">
-                <div className="icon-show">
-                  <div className="money">
-                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
-                  </div>
-                  <div className="day-lenght">
-                    <Ri24HoursFill /> 12 Day
-                  </div>
-                </div>
-                <div className="button-show">
-                  <a href="/"> Show more..</a>
-                </div>
-              </div>
-            </div>
-            <div className="box-4"></div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
