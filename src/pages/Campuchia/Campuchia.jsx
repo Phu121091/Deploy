dd2d0e3b679c5b3c4c80f49a73a0b8fd89c7686f
TrongVi <vutrongvi96@gmail.com>
2022-11-11 15:16:06 +0700

    5

---
+++ src/pages/Campuchia/Campuchia.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CSSModules from "react-css-modules";
+import "./Campuchia.css";
+const Campuchia = () => {
+  return (
+    <div>Campuchia</div>
+  )
+}
+
+export default Campuchia
